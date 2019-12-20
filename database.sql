cf891c86dba1d467e25618958ac7f48551d5b854
Harrison Nguyen <nguy2173@umn.edu>
2019-12-20 15:46:35 -0600

    Add joint table in database.sql file

--- database.sql
+++ database.sql
@@ -16,8 +16,11 @@ CREATE TABLE "genres" (
 -- You will need to create the junction table that stores the relationships between "movies" and "genres"
 -- This table will need to be populated with some data as well (INSERTS)
 -- Recall that this Junction Table will just be a table of ids!
-
-
+CREATE TABLE "movies-genres" (
+  "id" SERIAL PRIMARY KEY,
+  "movie_id" INT REFERENCES "movies",
+	"genre_id" INT REFERENCES "genres"
+);
 
 --------[ DATA! ]---------
 
@@ -55,3 +58,6 @@ VALUES
 ('Science Fiction'),
 ('Space-Opera'),
 ('Superhero');
+
+-- starter data with joint movies and genres
+INSERT INTO "movies-genres" ("movie_id", "genre_id") VALUES (4, 2), (3, 13), (5, 6), (13, 10), (9, 2);
